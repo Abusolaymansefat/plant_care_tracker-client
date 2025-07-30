73ff82b03a963e4a085721bbe356fc676d6fd842
Abusolay <sefat01625@gmail.com>
2025-07-30 21:36:20 +0600

    imager uploder mongo db thake data ann and deta image

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,10 +1,17 @@
 import AddPlantForm from "../../../components/Form/AddPlantForm";
 import { imageUpload } from "../../../api/utils";
 import axios from "axios";
+import useAuth from "../../../hooks/useAuth";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 const AddPlant = () => {
+  const { user } = useAuth();
+  const [isUploading, setIsUploading] = useState(false);
+
   const handleFromSbmit = async (e) => {
     e.preventDefault();
+    setIsUploading(true);
     const form = e.target;
     const name = form?.name?.value;
     const category = form?.category?.value;
@@ -12,25 +19,41 @@ const AddPlant = () => {
     const price = form?.price?.value;
     const quantity = form?.quantity?.value;
     const image = form?.image?.files[0];
-    const imageUrl =  await imageUpload(image);
+    const imageUrl = await imageUpload(image);
 
-    const plantData = {
-      name,
-      category,
-      description,
-      price,
-      quantity,
-      image: imageUrl,
-    };
-    console.log(plantData)
+    try {
+      const plantData = {
+        name,
+        category,
+        description,
+        price,
+        quantity,
+        image: imageUrl,
+        seller: {
+          name: user?.displayName,
+          email: user?.email,
+          image: user?.photoURL
+        },
+      };
 
-    const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/add-plant`, plantData)
-    console.log(data)
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_API_URL}/add-plant`,
+        plantData
+      );
+      toast.success("Plant Data Added Successfully !!!");
+      form.reset();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsUploading(false);
+    }
   };
+
   return (
     <div>
       {/* Form */}
-      <AddPlantForm handleFromSbmit={handleFromSbmit} />
+      <AddPlantForm handleFromSbmit={handleFromSbmit} isUploading={isUploading} />
     </div>
   );
 };
