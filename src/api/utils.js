9f61e3c72876189667f097dbfbc22180e369e52e
Abusolay <sefat01625@gmail.com>
2025-08-02 00:10:07 +0600

    user login time

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,18 +1,25 @@
 import axios from "axios";
 
-//uplode image and retant image url 
+//uplode image and retant image url
 
-export const imageUpload = async imageData => {
-     const imageFromData = new FormData();
-    imageFromData.append("image", imageData);
+export const imageUpload = async (imageData) => {
+  const imageFromData = new FormData();
+  imageFromData.append("image", imageData);
 
-    //upload image in imgbb server using post requst 
-        const { data } = await axios.post(
-          `https://api.imgbb.com/1/upload?key=${
-            import.meta.env.VITE_IMGBB_API_KEY
-          }`,
-          imageFromData
-        );
-    
-        return data?.data?.display_url;
-}
+  //upload image in imgbb server using post requst
+  const { data } = await axios.post(
+    `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`,
+    imageFromData
+  );
+
+  return data?.data?.display_url;
+};
+
+//seve or update user in db
+export const seveUserInDb = async (user) => {
+  const { data } = await axios.post(
+    `${import.meta.env.VITE_API_URL}/user`,
+    user
+  );
+  console.log(data)
+};
